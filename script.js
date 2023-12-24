ce5cf1d0d29c8d4835727bcb5dd6295a2048e5a6
Samuel Barros <samuel.barrosluna@outlook.com>
2023-12-24 02:22:56 -0300

    Filtro de Resuldados

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ const image = document.getElementById('img');
 btnGo.addEventListener('click', async (event) => {
     event.preventDefault()
     const result = await fetchAPI(characterId.value)
-    content.innerText = `${JSON.stringify(result, undefined, 2)}`
+    // content.textContent = `${JSON.stringify(result, undefined, 2)}`
+    content.textContent = `${JSON.stringify(buildResult(result), undefined, 2)}`
     image.src = `${result.image}`
 });
+
+const keys = ['name', "status", "species", "gender", "origin", "image", "episode"];
+
+const buildResult = (result) => {
+    const newObject = {};
+    keys.map((key) => document.getElementById(key))
+        .map((elem) => {
+            elem.checked && (newObject[elem.name] = result[elem.name])
+        })
+
+    return newObject
+}
 
 const fetchAPI = (value) => {
     const url = `https://rickandmortyapi.com/api/character/${value}`;
